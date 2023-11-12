3a6749c54cf46fddfb98ee8ddda8842fdc064e29
Uddeshya-UD <uddeshya31@gmail.com>
2023-11-12 09:53:11 +0530

    finished sign in and sign up pages

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,14 +6,17 @@ const Input = ({
     type = 'text',
     className = '' ,
     isRequired = false,
-    placeholder = ''
-}
+    placeholder = '',
+    value = '',
+    onChange = () => {},
+  }
 ) => {
   return (
     <div className='w-1/2'>
         <label for={name} className='black mb-2 text-sm font-medium text-gray-800'>{label}</label>
         <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-        focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 ${className}`} placeholder={placeholder} required={isRequired}/>
+        focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 ${className}`} placeholder={placeholder} required={isRequired}
+        value={value} onChange={onChange}/>
     </div>
   )
 }
--- src/modules/forms/index.js
+++ src/modules/forms/index.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../../components/Input/index';
 import Button from '../../components/Button';
 const Form = ({
-  isSignInPage = false
+  isSignInPage = true,
 }) => {
+
+ const [data, setdata] = useState({
+  ...(isSignInPage && {
+    fullName: ''
+  }),
+  email: '',
+  password: ''
+ })
+
+ console.log("data >> " ,data);
+ 
   return (
     <div className='bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center'>
        <div className='text-4xl font-bold'>Welcome {isSignInPage && 'Back'}</div>   
        <div className='text-xl font-light mb-14'>{isSignInPage ? 'Sign in to explore' : 'Sign up now to get started'}</div>  
-       {!isSignInPage &&  <Input label='Full Name' name="name" placeholder='Enter your fullname' className='mb-6'/>}
-       <Input label='Email Address' name="name" placeholder='Enter your email' className='mb-6'/>
-       <Input label='Password' name="name" placeholder='Enter your Password' className='mb-14'/>
-       <Button label={isSignInPage ? "Sign in" : "Sign up"} className='w-1/2 mb-4'/>
+       <form className="flex flex-col items-center w-full" onSubmit={(e) => console.log("Submitted")}>
+        {!isSignInPage &&  <Input label='Full Name' name="name" placeholder='Enter your fullname' className='mb-6' value={data.fullName} onChange={(e)=> setdata( { ...data, fullName: e.target.value})}/>}
+       <Input label='Email Address' type='email' name="name" placeholder='Enter your email' className='mb-6' value={data.email} onChange={(e) => setdata({...data,email: e.target.value})}/>
+       <Input label='Password' name="name" placeholder='Enter your Password' className='mb-14' value={data.password} onChange={(e) => setdata({...data,password: e.target.value})}/>
+       <Button label={isSignInPage ? "Sign in" : "Sign up"} className='w-1/2 mb-4' type='submit'/>
+       </form>
+
        <div>{isSignInPage ? "Didn't have an account" : "Already have an account" } <span className='text-primary custor-pointer underline'>{isSignInPage ? "Sign up" : "Sign in"}</span></div>
     </div>
   )
